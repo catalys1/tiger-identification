c9220cc0a6a1fc1e69f0f03f12c7242d9aa42c5d
Connor <catalys1@gmail.com>
2018-11-06 21:07:17 -0700

    Clean up main a little bit

--- identify/main.py
+++ identify/main.py
@@ -13,35 +13,6 @@ from types import SimpleNamespace
 DATA = Path('../data/')
 
 
-class AllPairContrastLoss(torch.nn.Module):
-
-    def __init__(self, m=1.0):
-        # we want the margin to be somewhat small. We need the contrast loss
-        # in order to avoid mode collapse, but we care most about pushing 
-        # same-class representations close together
-        super(AllPairContrastLoss, self).__init__()
-        self.m = m
-    
-    def forward(self, x, labels):
-        pairwise_dist_sq = torch.mm(x, x.t())
-        squared_norm = pairwise_dist_sq.diag()
-        pairwise_dist_sq = (
-            squared_norm.view(1, -1) + 
-            squared_norm.view(-1, 1) - 
-            2 * pairwise_dist_sq)
-        pairwise_dist_sq.clamp_(min=0.0)
-
-        same_label = labels.view(-1, 1).eq(labels)
-
-        pairs_same = pairwise_dist_sq[same_label]
-        pairs_diff = pairwise_dist_sq[(1 - same_label)]
-
-        loss_pos = pairs_same.mean()
-        loss_neg = torch.clamp(self.m - pairs_diff.sqrt(), min=0).pow(2).mean()
-        loss = loss_pos + loss_neg
-        return loss
-
-
 def get_data(data, batch_size=40, num_workers=4):
     largs = dict(
         batch_size=batch_size,
@@ -89,9 +60,7 @@ def setup(cfg, args):
     data = cfg.data.data(**cfg.data.args)
     loaders = get_data(data, args.batch_size, num_workers=8)
 
-    templates = torch.load(cfg.other['templates']).cuda()
     net = dnnutil.load_model(cfg.model.model, args.model, **cfg.model.args)
-    net.set_ssd_kernels(templates)
 
     optim = torch.optim.Adam(net.parameters(), lr=args.lr)
     loss_fn = cfg.loss.loss(**cfg.loss.args)
@@ -101,12 +70,11 @@ def setup(cfg, args):
     return state
 
 
-
 def main():
-    parser = dnnutil.config_parser()
+    parser = dnnutil.config_parser(run_dir='../data/training/')
     args = parser.parse_args()
 
-    manager = dnnutil.ConfigManager(root='../data/training/', run_num=args.rid)
+    manager = dnnutil.ConfigManager(root=args.run_dir, run_num=args.rid)
     cfg = manager.setup(args)
     
     state = setup(cfg, args)
@@ -115,7 +83,6 @@ def main():
         t = time.time()
         state.trainer.train(state.loaders[0], e)
         state.trainer.eval(state.loaders[1], e)
-        state.trainer.plot_dist()
 
         t = time.time() - t
         stats = state.trainer.get_stats()
